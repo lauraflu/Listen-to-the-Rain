32c95bdd82324891bc6cf8422ff485e385cbe501
Laura Flueratoru <flueratorulaura@gmail.com>
2015-04-22 23:43:20 +0300

    Test files + calc deviation

--- sound_levels_LED/sound_levels_LED.ino
+++ sound_levels_LED/sound_levels_LED.ino
@@ -47,26 +47,7 @@ void loop()
     }
     
     peak_to_peak = max_level - min_level;
-    // The microphone's Vcc is connected to Arduino's 3.3V. If you choose to connect it to 5V
-    // remember to also change the value below, for a correct conversion. 
-    vpp = (peak_to_peak * 3.3) / 1023;
-    
-    Serial.println(vpp);
-    
-    if (vpp < 1) {
-        light_LED(G_led);
-    } else if ((vpp > 1) && (vpp < 1.8)) {
-        light_LED(Y_led);
-    } else if (vpp > 1.8) {
-        light_LED(R_led);
-    }
-}
-
-void light_LED (int LED)
-{
-    digitalWrite(LED, HIGH);
-    delay(150);
-    digitalWrite(LED, LOW);
+    Serial.println(peak_to_peak);
 }
 
 
---
+++ sound_levels_arduino/sound_levels_arduino.ino
@@ -0,0 +1,47 @@
+const int sample_window = 50; // this is the size of the sample window in ms
+const int ref_val = 230; // this value is a reference for the p2p values
+// the compression algorithm will be applied to the deviations from this value
+const int no_samples = 150; // after 150 received samples, we compress & send them
+unsigned int sample; // stores the level of a measured sample
+unsigned int cnt_sample = 0;
+
+void setup()
+{
+    Serial.begin(9600);
+}
+
+void loop()
+{
+    // millis() is a function that returns the number of milliseconds since the program
+    // started, as an unsigned long
+    unsigned long loop_start = millis();
+    unsigned int peak_to_peak = 0; // peak to peak level
+    double vpp = 0; // peak to peak voltage
+    unsigned int max_level = 512;
+    unsigned int min_level = 512;
+
+    if (cnt_sample < 150) {
+        while (millis() - loop_start < sample_window) {        
+            sample = analogRead(0); // the mic is connected to Arduino's Analog Input pin no. 0
+            if (sample > max_level) {
+                max_level = sample;
+            } else if (sample < min_level) {
+                min_level = sample;
+            }
+        }
+        Serial.print("\n");
+        Serial.print("cnt_sample is: ");
+        Serial.print(cnt_sample);
+        Serial.print("\n");
+        cnt_sample++;
+        peak_to_peak = max_level - min_level;
+        Serial.print("\n");
+        Serial.print("peak_to_peak is: ");
+        Serial.print(peak_to_peak);
+        Serial.print("\n");
+    } else {
+        Serial.println("Apply compression algorithm here.");
+    }
+}
+
+
