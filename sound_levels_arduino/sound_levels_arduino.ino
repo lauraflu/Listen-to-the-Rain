cf3f7bbf3b787c71ca9efd32bd80bd5911def012
Laura Flueratoru <flueratorulaura@gmail.com>
2015-04-22 23:55:14 +0300

    Calc deviation & store in vector

--- sound_levels_arduino/sound_levels_arduino.ino
+++ sound_levels_arduino/sound_levels_arduino.ino
@@ -2,6 +2,7 @@ const int sample_window = 50; // this is the size of the sample window in ms
 const int ref_val = 230; // this value is a reference for the p2p values
 // the compression algorithm will be applied to the deviations from this value
 const int no_samples = 150; // after 150 received samples, we compress & send them
+int p2p_deviation[150];
 unsigned int sample; // stores the level of a measured sample
 unsigned int cnt_sample = 0;
 
@@ -20,7 +21,7 @@ void loop()
     unsigned int max_level = 512;
     unsigned int min_level = 512;
 
-    if (cnt_sample < 150) {
+    if (cnt_sample < no_samples) {
         while (millis() - loop_start < sample_window) {        
             sample = analogRead(0); // the mic is connected to Arduino's Analog Input pin no. 0
             if (sample > max_level) {
@@ -39,8 +40,14 @@ void loop()
         Serial.print("peak_to_peak is: ");
         Serial.print(peak_to_peak);
         Serial.print("\n");
+        p2p_deviation[cnt_sample] = ref_val - peak_to_peak; // calculate deviation and store in vector
+        Serial.print("\n");
+        Serial.print("deviation is: ");
+        Serial.print(p2p_deviation[cnt_sample]);
+        Serial.print("\n");
     } else {
-        Serial.println("Apply compression algorithm here.");
+        Serial.println("Apply compression algorithm here & send.");
+        cnt_sample = 0;
     }
 }
 
