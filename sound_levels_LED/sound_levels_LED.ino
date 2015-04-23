dfdea7b1e4d29265d3e9754b3a313db25e034f3b
Laura Flueratoru <flueratorulaura@gmail.com>
2015-04-23 14:35:37 +0300

    Going back to the old version

--- sound_levels_LED/sound_levels_LED.ino
+++ sound_levels_LED/sound_levels_LED.ino
@@ -1,4 +1,3 @@
-/* 
 Measuring sound levels in a sample window of 50ms (T = 50ms => f = 20Hz)
 and lighting LEDs according to these levels.
 Humans hear frequencies from 20Hz up to 20kHz (although the upper limit varies 
@@ -36,7 +35,6 @@ void loop()
     double vpp = 0; // peak to peak voltage
     unsigned int max_level = 512;
     unsigned int min_level = 512;
-
     while (millis() - loop_start < sample_window) {
         sample = analogRead(0); // the microphone is connected to Arduino's Analog Input pin no. 0
         if (sample > max_level) {
@@ -47,7 +45,24 @@ void loop()
     }
     
     peak_to_peak = max_level - min_level;
-    Serial.println(peak_to_peak);
+    // The microphone's Vcc is connected to Arduino's 3.3V. If you choose to connect it to 5V
+    // remember to also change the value below, for a correct conversion. 
+    vpp = (peak_to_peak * 3.3) / 1023;
+    
+    Serial.println(vpp);
+    
+    if (vpp < 1) {
+        light_LED(G_led);
+    } else if ((vpp > 1) && (vpp < 1.8)) {
+        light_LED(Y_led);
+    } else if (vpp > 1.8) {
+        light_LED(R_led);
+    }
 }
 
-
+void light_LED (int LED)
+{
+    digitalWrite(LED, HIGH);
+    delay(150);
+    digitalWrite(LED, LOW);
+}
