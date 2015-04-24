d7021ad72808cb5078bf1d236bd1604eb856d06d
Laura Flueratoru <flueratorulaura@gmail.com>
2015-04-24 20:10:24 +0300

    Calculate powers

--- sound_levels_arduino/sound_levels_arduino.ino
+++ sound_levels_arduino/sound_levels_arduino.ino
@@ -1,10 +1,8 @@
 const int sample_window = 50; // this is the size of the sample window in ms
-const int ref_val = 230; // this value is a reference for the p2p values
-// the compression algorithm will be applied to the deviations from this value
 const int no_samples = 150; // after 150 received samples, we compress & send them
-int p2p_deviation[150];
-unsigned int sample; // stores the level of a measured sample
-unsigned int cnt_sample = 0;
+float power[150];
+float sample; // stores the level of a measured sample
+float sample_power = 0;
 
 void setup()
 {
@@ -15,36 +13,39 @@ void loop()
 {
     // millis() is a function that returns the number of milliseconds since the program
     // started, as an unsigned long
-    unsigned long loop_start = millis();
-    unsigned int peak_to_peak = 0; // peak to peak level
-    double vpp = 0; // peak to peak voltage
-    unsigned int max_level = 512;
-    unsigned int min_level = 512;
+    delay(5000);
+    unsigned int cnt_sample = 0; // iterates through the vector
+    unsigned int cnt_mean = 1; // for calculating the mean power
 
     if (cnt_sample < no_samples) {
-        while (millis() - loop_start < sample_window) {        
+        unsigned long loop_start = millis();
+        Serial.println("loopstart: ");
+        Serial.println(loop_start);
+        while (millis() - loop_start < sample_window) {
+            Serial.println(millis());
             sample = analogRead(0); // the mic is connected to Arduino's Analog Input pin no. 0
-            if (sample > max_level) {
-                max_level = sample;
-            } else if (sample < min_level) {
-                min_level = sample;
-            }
+            // Serial.println(sample);
+            sample = ((sample - 512) * 5) / 1023; // value in volts
+            sample_power = sample_power + (sample * sample);
+            
+            // Serial.println(sample_power);
+            Serial.println(cnt_mean);
+            cnt_mean++;
         }
+        
+        power[cnt_sample] = sample_power / cnt_mean;
+        
         Serial.print("\n");
-        Serial.print("cnt_sample is: ");
-        Serial.print(cnt_sample);
-        Serial.print("\n");
+        Serial.print("Power per window of 5s: ");
+        Serial.print(power[cnt_sample]);
+        Serial.print("\n");        
+        
+        cnt_mean = 0;
+        sample_power = 0;
         cnt_sample++;
-        peak_to_peak = max_level - min_level;
-        Serial.print("\n");
-        Serial.print("peak_to_peak is: ");
-        Serial.print(peak_to_peak);
-        Serial.print("\n");
-        p2p_deviation[cnt_sample] = ref_val - peak_to_peak; // calculate deviation and store in vector
-        Serial.print("\n");
-        Serial.print("deviation is: ");
-        Serial.print(p2p_deviation[cnt_sample]);
-        Serial.print("\n");
+        Serial.print("cnt_sample: ");
+        Serial.print(cnt_sample);
+        
     } else {
         Serial.println("Apply compression algorithm here & send.");
         cnt_sample = 0;
