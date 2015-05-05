26668673a8248b050b9639851487c122c2c15c1a
Laura Flueratoru <flueratorulaura@gmail.com>
2015-05-05 22:20:14 +0300

    Sampling 64us with print of first 800 values of sec

--- sound_levels_arduino/sound_levels_arduino.ino
+++ sound_levels_arduino/sound_levels_arduino.ino
@@ -1,55 +1,63 @@
-const int sample_window = 50; // this is the size of the sample window in ms
-const int no_samples = 150; // after 150 received samples, we compress & send them
-float power[150];
-float sample; // stores the level of a measured sample
-float sample_power = 0;
+#define SIZE 800
+
+const int sample_window = 1000; // this is the size of the sample window in ms
+int sample; // stores the level of a measured sample
+unsigned int i = 0;
+double power = 0;
+int sample_vector[SIZE];
+
+// Define various ADC prescaler
+// The ADC clock needs  to be between 50 kHz and 200  kHz for 10 bit accuracy.
+// With the 128 prescaler, the ADC's clock will run at 125 kHz. A normal 
+// conversion in the ADC takes 13 clock cycles, so the sample speed will be 
+// 125 kHz / 13 ~= 9600 Hz => maximum 9600 samples / sec. 
+// With the 64 prescaler, the ADC's clock will run at 250 kHz, so the sample
+// speed will be 250 kHz / 13 ~= 19 000 Hz, so we'll get about as many samples/sec.
+// We the 64 prescaler, we can get a samples once every 60 - 64 us.
+// In reality, depending on what other operations we perform in the sampling
+// loop, we'll have less samples/sec.
+
+const unsigned char PS_64 = (1 << ADPS2) | (1 << ADPS1);
+const unsigned char PS_128 = (1 << ADPS2) | (1 << ADPS1) | (1 << ADPS0);
 
 void setup()
 {
     Serial.begin(9600);
+    
+    // set up the ADC
+    ADCSRA &= ~PS_128;  // remove bits set by Arduino library
+    ADCSRA |= PS_128;    // set our own prescaler to 64 
 }
 
 void loop()
 {
-    // millis() is a function that returns the number of milliseconds since the program
-    // started, as an unsigned long
-    delay(5000);
-    unsigned int cnt_sample = 0; // iterates through the vector
-    unsigned int cnt_mean = 1; // for calculating the mean power
+  // millis() is a function that returns the number of milliseconds since the program
+  // started, as an unsigned long
+  
+//  Serial.println("Say something");
 
-    if (cnt_sample < no_samples) {
-        unsigned long loop_start = millis();
-        Serial.println("loopstart: ");
-        Serial.println(loop_start);
-        while (millis() - loop_start < sample_window) {
-            Serial.println(millis());
-            sample = analogRead(0); // the mic is connected to Arduino's Analog Input pin no. 0
-            // Serial.println(sample);
-            sample = ((sample - 512) * 5) / 1023; // value in volts
-            sample_power = sample_power + (sample * sample);
-            
-            // Serial.println(sample_power);
-            Serial.println(cnt_mean);
-            cnt_mean++;
-        }
-        
-        power[cnt_sample] = sample_power / cnt_mean;
-        
-        Serial.print("\n");
-        Serial.print("Power per window of 5s: ");
-        Serial.print(power[cnt_sample]);
-        Serial.print("\n");        
-        
-        cnt_mean = 0;
-        sample_power = 0;
-        cnt_sample++;
-        Serial.print("cnt_sample: ");
-        Serial.print(cnt_sample);
-        
-    } else {
-        Serial.println("Apply compression algorithm here & send.");
-        cnt_sample = 0;
-    }
+  i = 0;
+  power = 0;
+  unsigned long loop_start = millis();
+  while (millis() - loop_start < sample_window) {
+    sample = analogRead(0) - 512; // the mic is connected to Arduino's AI pin no. 0
+    // we calculate the values relative to the level 512
+    if (i < SIZE)
+      sample_vector[i] = sample;
+    
+    power += sample * sample;
+    i++;
+  }
+   
+  power = power / i + 1;
+  Serial.println("number samples: ");   
+  Serial.println(i);
+  /*
+  for (i = 0; i < SIZE; i++) {
+    Serial.println(sample_vector[i]);
+  }
+  */
+  // send the value of the power over wireless to the second Arduino 
 }
 
 
